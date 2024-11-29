bfd60e22f77165c4ece3163324cf113b12887192
PrAsHaNtH01532 <151920029+PrAsHaNtH01532@users.noreply.github.com>
2024-11-29 10:00:59 +0530

    Add files via upload

---
+++ ab.js
@@ -0,0 +1,24 @@
+// // const fs=require("fs");
+// // function print(err,data){
+// //     console.log(data);
+// // }
+// // fs.readFile("a.txt","utf-8",print);
+// // fs.readFile("b.txt","utf-8",print);
+
+// // console.log("abd");
+
+
+// console.log("hi");
+// function timeout(){
+//     console.log("waited for 5 secs?");
+// }
+// setTimeout(timeout,1000);
+// console.log("bye then!!!!!!");
+// p=0;
+// for(let i=0;i<1000;i++){
+//     p=p+1;
+// }
+// console.log("done!!!!!!!!!");
+console.log("5"-2);
+console.log("5"+2);
+console.log("5"*2);
